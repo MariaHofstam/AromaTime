c2883ca0c9e3e647e495058ce4196c4aeb8d0dcb
Maria <45755355+MariaHofstam@users.noreply.github.com>
2019-03-25 09:55:55 +0100

    First commit

---
+++ src/main/java/se/lexicon/maria/aromatime/AromatimeApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.maria.aromatime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AromatimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AromatimeApplication.class, args);
+	}
+
+}
